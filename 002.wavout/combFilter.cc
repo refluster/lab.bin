727d8cbd67ae3a4ccc6987d10071996ea840f790
refluster <refluster@gmail.com>
2016-01-17 12:42:16 +0900

    implement comb filter

---
+++ 002.wavout/combFilter.cc
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "combFilter.h"
+
+CombFilter::CombFilter(size_t size, float g) {
+	this->size = size;
+	this->g = g;
+	this->buf = (float*)calloc(size, sizeof(float));
+	this->pRead = this->pWrite = this->buf;
+};
+
+CombFilter::~CombFilter() {
+	free(this->buf);
+};
+
+void CombFilter::enqdeq(float *in, float *out) {
+	*out = *this->pRead;
+
+	*this->pWrite = *in + this->g * (*this->pRead);
+
+	++this->pWrite;
+	++this->pRead;
+	if (this->pWrite >= this->buf + this->size) {
+		this->pWrite = this->buf;
+	}
+	if (this->pRead >= this->buf + this->size) {
+		this->pRead = this->buf;
+	}
+};
+
+#ifdef TEST
+int main() {
+	CombFilter *cf = new CombFilter(4, .5f);
+	float in[30] = {1, 2, 3, 4, 5, 6, 0, 0, 0, 0};
+	float out;
+	
+	for (int i = 0; i < 30; i++) {
+		cf->enqdeq(&in[i], &out);
+
+		printf("-- %2d %3f\n", i, out);
+	}
+}
+#endif
---
+++ 002.wavout/combFilter.h
@@ -0,0 +1,16 @@
+#ifndef _COMB_FILTER_H_
+#define _COMB_FILTER_H_
+
+class CombFilter {
+	size_t size;
+	float g;
+	float *pRead, *pWrite;
+	float *buf;
+
+public:
+	CombFilter(size_t size, float g);
+	~CombFilter();	
+	void enqdeq(float *in, float *out);
+};
+
+#endif
