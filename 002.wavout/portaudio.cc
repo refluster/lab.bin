b6372fa862a6cc287c0b3c4f19934c6def44163b
refluster <refluster@gmail.com>
2016-01-17 11:04:24 +0900

    rename source files

--- 002.wavout/portaudio.cc
+++ 002.wavout/portaudio.cc
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <math.h>
 #include <portaudio.h>
-#include "sndfile.h"
+#include "wavReader.h"
 
 #define NUM_SECONDS 3
 #define SAMPLE_RATE 44100
--- 002.wavout/wavReader.cc
+++ 002.wavout/wavReader.cc
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sndfile.h>
-#include "sndfile.h"
+#include "wavReader.h"
 
 int WavReader::init() {
 	// Open sound file
