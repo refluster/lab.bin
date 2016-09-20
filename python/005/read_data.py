9c7a03ea73e20c2668cbd27a990e86d54b653c07
refluster <refluster@gmail.com>
2016-09-20 21:49:37 +0900

    read data as np.array

--- python/005/read_data.py
+++ python/005/read_data.py
@@ -2,14 +2,15 @@ import csv
 import numpy as np
 
 def read_csv():
-    f = open('data.csv', 'r')
-    dataReader = csv.reader(f)
-    
     data = []
-    for d in dataReader:
-        rssi = float(d[1])
-        data.append([rssi])
-    
+    for fname in ['data.csv', 'data2.csv']:
+        f = open(fname, 'r')
+        dataReader = csv.reader(f)
+        d = []
+        for c in dataReader:
+            rssi = float(c[1])
+            d.append([rssi])
+        data.append(np.array(d))
     return data
             
 def read():
